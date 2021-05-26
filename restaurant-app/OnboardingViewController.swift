85c6acf1fb43e857ed63b7bfc866f02f235fcb10
Sherry1481 <shareyscorpi1@gmail.com>
2021-05-27 01:10:52 +0500

    c

---
+++ restaurant-app/OnboardingViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  restaurant-app
+//
+//  Created by sherry on 27/05/2021.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+    }
+
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView.collectionViewLayout = layout
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.isPagingEnabled = true
+        collectionView.showsVerticalScrollIndicator = false
+    }
+    
+    
+}
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+
+
+
+}
